d4c322873e30c5ccf3f9aa193f5a51ce37b869c2
AlonCohen96 <aloncohen36@gmail.com>
2023-12-02 15:53:47 +0100

    Fixed parameters form

--- src/ScalableSpec.jsx
+++ src/ScalableSpec.jsx
@@ -25,7 +25,7 @@ const SCROLL_STEP_RATIO = 0.1
 const LABEL_COLOR = "#00FF00"
 const LABEL_COLOR_HOVERED = "#f3e655"
 
-function ScalableSpec( { response, audioFileName, importedLabels, activeClustername, spectrogramIsLoading, passSpectrogramIsLoadingToApp, specType, nfft, nbins, parameters }) {
+function ScalableSpec( { response, audioFileName, importedLabels, activeClustername, spectrogramIsLoading, passSpectrogramIsLoadingToApp, specType, nfft, binsPerOctave, parameters }) {
     // General
     const [audioDuration, setAudioDuration] = useState(0);
     const [audioId, setAudioId] = useState(null);
@@ -72,14 +72,16 @@ function ScalableSpec( { response, audioFileName, importedLabels, activeClustern
 
     const getAudioClipSpec = async (startTime, duration, spectrogramType) => {
         const path = import.meta.env.VITE_BACKEND_SERVICE_ADDRESS+'get-audio-clip-spec'
+        console.log('bins: ' + binsPerOctave)
+        console.log('nfft: ' + nfft)
         const requestParameters = {
             //...parameters,
             audio_id: audioId,
             start_time: startTime,
             clip_duration: duration,
             spec_cal_method: spectrogramType,
-            nfft: nfft,
-            n_bins: nbins,
+            n_fft: nfft,
+            bins_per_octave: binsPerOctave,
         }
 
         const response = await axios.post(path, requestParameters)
@@ -909,7 +911,7 @@ function ScalableSpec( { response, audioFileName, importedLabels, activeClustern
             }
 
             getSpecAndAudioArray()
-        }, [currentStartTime, clipDuration]
+        }, [currentStartTime, clipDuration, nfft, binsPerOctave]
     );
 
     // When user changed spectrogram type

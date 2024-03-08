e701ee19a8b3c2a4dbaaae23ed723348212d0b37
AlonCohen96 <aloncohen36@gmail.com>
2024-03-08 16:56:36 +0100

    Fixed issue when uploading file manually after using url parameter, the gobalAudioDuration wouldn't update

--- src/FileUpload.jsx
+++ src/FileUpload.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useEffect} from "react";
 import axios from "axios";
+import {useLocation} from "react-router-dom";
 
 function FileUpload( { passResponseToScalableSpec, passSpectrogramIsLoadingToScalableSpec, passTrackDurationToApp, deletePreviousTrackDurationInApp, previousAudioDuration} ) {
+
+    const location = useLocation();
 
     const handleFileChange = (event) => {
         const file = event.target.files[0];
@@ -24,6 +27,7 @@ function FileUpload( { passResponseToScalableSpec, passSpectrogramIsLoadingToSca
         const path = import.meta.env.VITE_BACKEND_SERVICE_ADDRESS+'upload'
         try {
             const response = await axios.post(path, formData)
+            console.log('prev duration is: ' + previousAudioDuration)
             passResponseToScalableSpec( response )
             deletePreviousTrackDurationInApp( previousAudioDuration ) // Remove outdated track duration of the previous file in the App component
             passTrackDurationToApp( response.data.audio_duration )
@@ -31,6 +35,47 @@ function FileUpload( { passResponseToScalableSpec, passSpectrogramIsLoadingToSca
             console.error("Error uploading file:", error)
         }
     }
+
+    // When url parameter is added into the searchbar
+    useEffect( () => {
+        let ignore = false
+
+        const queryParams = new URLSearchParams(location.search);
+        const audioUrlParam = queryParams.get('audio_url');
+
+        const uploadFileByURL = async (audioURL) => {
+            passSpectrogramIsLoadingToScalableSpec( true )
+            const path = import.meta.env.VITE_BACKEND_SERVICE_ADDRESS+'upload-by-url'
+            const requestParameters = {
+                //audio_url: 'https://storage.googleapis.com/callbase_bucket/XC633481-RFW_.mp3?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=callbase-storage-management%40callbase-395411.iam.gserviceaccount.com%2F20240222%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240222T135207Z&X-Goog-Expires=604800&X-Goog-SignedHeaders=host&X-Goog-Signature=171057da4032d1ee38946369ab9b0bd090539b7d258c7fb1f9b057120b5e6ed9bcdb9f585d0648852e78b10baee07882f813e1278d227dad2b17f0410ae91e4c0a472210485a8d085c6ffa0f3ddfe7f038fee2c9d0089b0980d44ea00cf32d58542bbca37f2bf0cfccc1220ee26ce242340f2fbc238f9b8c0d85bc4d4e975d926e93c58313bc60bb0d40620dfaaeea85068d17282a2e94c7dae6d87b4eb88ffd8794c53a41e936e94df8f59bf28d76dc7042671053fb80f95a94ad2e60bf168784941c52dcbeef7b4575d15649a971e968b8b934f2eba5a2f4862a35727b75d76b7d672791cfc1837a32cd421e80b016722e446bc8b981a0fdd548636fdc8843',
+                //audio_url: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav'
+                audio_url: audioURL
+            }
+
+            try {
+                const response = await axios.post(path, requestParameters)
+                if (!ignore){
+                    passResponseToScalableSpec( response )
+                    deletePreviousTrackDurationInApp( previousAudioDuration ) // Remove outdated track duration of the previous file in the App component
+                    passTrackDurationToApp( response.data.audio_duration )
+                }
+            } catch (error){
+                passSpectrogramIsLoadingToScalableSpec( false )
+                console.error("Error uploading file:", error)
+                alert('Error while uploading. Make sure the URL to the file is correct and try again.')
+            }
+        }
+
+        if (audioUrlParam) {
+            const decodedURL = decodeURIComponent(audioUrlParam)
+            uploadFileByURL(decodedURL)
+        }
+
+        return () => {
+            ignore = true
+        }
+
+    }, [location])
 
     return (
         <div>
--- src/ScalableSpec.jsx
+++ src/ScalableSpec.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import Box from "@mui/material/Box";
 import LinearProgress from "@mui/material/LinearProgress";
@@ -110,7 +109,6 @@ function ScalableSpec(
     const [parameters, setParameters] = useState({
         spec_cal_method: 'log-mel'
     })
-    const location = useLocation();
 
     // Label Canvas
     const labelCanvasRef = useRef(null)
@@ -1342,21 +1340,7 @@ function ScalableSpec(
         setWhisperSegIsLoading(false)
     }
 
-    const uploadFileByURL = async (audioURL) => {
-        const path = import.meta.env.VITE_BACKEND_SERVICE_ADDRESS+'upload-by-url'
-        const requestParameters = {
-            //audio_url: 'https://storage.googleapis.com/callbase_bucket/XC633481-RFW_.mp3?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=callbase-storage-management%40callbase-395411.iam.gserviceaccount.com%2F20240222%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240222T135207Z&X-Goog-Expires=604800&X-Goog-SignedHeaders=host&X-Goog-Signature=171057da4032d1ee38946369ab9b0bd090539b7d258c7fb1f9b057120b5e6ed9bcdb9f585d0648852e78b10baee07882f813e1278d227dad2b17f0410ae91e4c0a472210485a8d085c6ffa0f3ddfe7f038fee2c9d0089b0980d44ea00cf32d58542bbca37f2bf0cfccc1220ee26ce242340f2fbc238f9b8c0d85bc4d4e975d926e93c58313bc60bb0d40620dfaaeea85068d17282a2e94c7dae6d87b4eb88ffd8794c53a41e936e94df8f59bf28d76dc7042671053fb80f95a94ad2e60bf168784941c52dcbeef7b4575d15649a971e968b8b934f2eba5a2f4862a35727b75d76b7d672791cfc1837a32cd421e80b016722e446bc8b981a0fdd548636fdc8843',
-            //audio_url: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav'
-            audio_url: audioURL
-        }
 
-        const newResponse = await axios.post(path, requestParameters)
-
-        console.log(newResponse.data)
-        setResponse( newResponse )
-        deletePreviousTrackDurationInApp( response? response.data.audio_duration : undefined ) // Remove outdated track duration of the previous file in the App component
-        passTrackDurationToApp( newResponse.data.audio_duration )
-    }
 
 
     /* ++++++++++++++++++ UseEffect Hooks ++++++++++++++++++ */
@@ -1417,16 +1401,6 @@ function ScalableSpec(
 
     }, [response, globalAudioDuration] )
 
-    // When url parameter is added into the searchbar
-    useEffect( () => {
-        const queryParams = new URLSearchParams(location.search);
-        const audioUrlParam = queryParams.get('audio_url');
-
-        if (audioUrlParam) {
-            const decodedURL = decodeURIComponent(audioUrlParam)
-            uploadFileByURL(decodedURL)
-        }
-    }, [location])
 
     return (
         <div
@@ -1504,7 +1478,6 @@ function ScalableSpec(
                         >
                             Call WhisperSeg
                         </button>
-                        <button onClick={drawCurvedOffset}>Draw curved line</button>
                         <div className='audio-controls'>
                             <button
                                 onClick={getAudio}

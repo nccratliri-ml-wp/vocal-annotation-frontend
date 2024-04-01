0c0ce99749e101828d0d857e34bca6f88c222830
AlonCohen96 <aloncohen36@gmail.com>
2024-04-01 17:13:25 +0200

    Redesigned AnnotationLabels Component Added delete method, custom species name features

--- src/AnnotationLabels.jsx
+++ src/AnnotationLabels.jsx
@@ -12,12 +12,16 @@ class Species {
 
 function AnnotationLabels () {
 
+    const [newSpeciesInputFieldText, setNewSpeciesInputFieldText] = useState('')
+
     const [speciesArray, setSpeciesArray] = useState([
-        new Species(nanoid(),'Unknown', ['Unknown'], ['Unknown'])
+        new Species(nanoid(),'Unknown Species', ['Unknown'], ['Unknown'])
     ])
 
-    const addNewSpecies = () => {
-        setSpeciesArray( prevState => [...prevState, new Species(nanoid(), 'New Species', ['Unknown'], ['Unknown'])] )
+    const addNewSpecies = (event) => {
+        event.preventDefault()
+        setSpeciesArray( prevState => [...prevState, new Species(nanoid(), newSpeciesInputFieldText, ['Unknown'], ['Unknown'])] )
+        setNewSpeciesInputFieldText('')
     }
 
     const addNewIndividual = (selectedID) => {
@@ -50,50 +54,111 @@ function AnnotationLabels () {
         setSpeciesArray(modifiedSpeciesArray)
     }
 
+    const deleteSpecies = (selectedID) => {
+        const modifiedSpeciesArray = speciesArray.filter(speciesObject => speciesObject.id !== selectedID)
+        setSpeciesArray(modifiedSpeciesArray)
+    }
+
+    const deleteIndividual = (event, selectedID, selectedIndividual) => {
+        event.preventDefault()
+
+        const modifiedSpeciesArray = speciesArray.map(speciesObject => {
+            if (speciesObject.id === selectedID) {
+                const updatedIndividuals = speciesObject.individuals.filter( individual => individual !== selectedIndividual)
+                return {
+                    ...speciesObject,
+                    individuals: updatedIndividuals
+                }
+            } else {
+                return speciesObject
+            }
+        })
+
+        setSpeciesArray(modifiedSpeciesArray)
+    }
+
+    const deleteClustername = (event, selectedID, selectedClustername) => {
+        event.preventDefault()
+
+        const modifiedSpeciesArray = speciesArray.map(speciesObject => {
+            if (speciesObject.id === selectedID) {
+                const updatedClustername = speciesObject.clusternames.filter( clustername => clustername !== selectedClustername)
+                return {
+                    ...speciesObject,
+                    clusternames: updatedClustername
+                }
+            } else {
+                return speciesObject
+            }
+        })
+
+        setSpeciesArray(modifiedSpeciesArray)
+    }
+
 
     return(
         <div id='annotation-labels-container'>
-            <button onClick={addNewSpecies}>Add New Species</button>
-            {
-                speciesArray.map( species =>
-                    <div
-                        id={species.id}
-                        key={species.id}
-                        className='species'
-                    >
-                        {species.name}
 
-                        <div className='individual-btn-container'>
+            <div id='annotation-labels-menu'>
+
+                {
+                    speciesArray.map( species =>
+                        <div
+                            id={species.id}
+                            key={species.id}
+                            className='species'
+                        >
+                            {species.name}
+                            {speciesArray.indexOf(species) !== 0 && <button onClick={() => deleteSpecies(species.id)}>🗑️</button>}
+
+                            <div className='individual-btn-container'>
+                                {
+                                    species.individuals.map( individual =>
+                                        <div
+                                            key={nanoid()}
+                                            className='individual-btn'
+                                            onContextMenu={ (event) => deleteIndividual(event, species.id, individual)}
+                                        >
+                                            {individual}
+                                        </div>
+                                    )
+                                }
+                                <button onClick={() => addNewIndividual(species.id)}>Add new Individual</button>
+                            </div>
+
+                            <div className='clustername-btn-container'>
                             {
-                                species.individuals.map( individual =>
+                                species.clusternames.map( clustername =>
                                     <div
                                         key={nanoid()}
-                                        className='individual-btn'
+                                        className='clustername-btn'
+                                        onContextMenu={ (event) => deleteClustername(event, species.id, clustername)}
                                     >
-                                        {individual}
+                                        {clustername}
                                     </div>
                                 )
                             }
-                            <button onClick={() => addNewIndividual(species.id)}>Add new Individual</button>
-                        </div>
+                                <button onClick={() => addNewClustername(species.id)}>Add new Clustername</button>
+                            </div>
 
-                        <div className='clustername-btn-container'>
-                        {
-                            species.clusternames.map( clustername =>
-                                <div
-                                    key={nanoid()}
-                                    className='clustername-btn'
-                                >
-                                    {clustername}
-                                </div>
-                            )
-                        }
-                            <button onClick={() => addNewClustername(species.id)}>Add new Clustername</button>
                         </div>
+                    )
+                }
 
-                    </div>
-                )
-            }
+            </div>
+
+            <form onSubmit={addNewSpecies}>
+                <input
+                    type='text'
+                    required='required'
+                    pattern='^[^,]{1,30}$'
+                    title='No commas allowed. Max length 30 characters'
+                    value={newSpeciesInputFieldText}
+                    placeholder='Add a new Species'
+                    onChange={ (event) => setNewSpeciesInputFieldText(event.target.value) }
+                />
+            </form>
+
         </div>
     )
 }

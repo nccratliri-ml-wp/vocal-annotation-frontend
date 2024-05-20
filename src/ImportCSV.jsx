d44d50ed2021fee5cfa9582da6c67949b139871c
AlonCohen96 <aloncohen36@gmail.com>
2024-05-20 14:10:09 +0200

    Decreased top bar size, gained additional space

--- src/ImportCSV.jsx
+++ src/ImportCSV.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import IconButton from "@material-ui/core/IconButton"
 import Tooltip from "@material-ui/core/Tooltip"
 import UploadFileIcon from "@mui/icons-material/UploadFile"
-import { icon } from "./styles.js"
+import {globalControlsBtn, icon} from "./styles.js"
 
 function ImportCSV( {passCsvImportedLabelsToApp} ) {
 
@@ -78,7 +78,10 @@ function ImportCSV( {passCsvImportedLabelsToApp} ) {
     return (
         <Tooltip title="Import CSV">
             <div style={{display: 'inline'}}>
-                <IconButton onClick={handleClick}>
+                <IconButton
+                    style={globalControlsBtn}
+                    onClick={handleClick}
+                >
                     <UploadFileIcon style={icon} />
                 </IconButton>
                 <input

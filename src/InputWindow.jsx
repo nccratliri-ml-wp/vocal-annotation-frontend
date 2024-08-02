1bbe4aee35780398c493e1d4f5b390b741603fae
AlonCohen96 <aloncohen36@gmail.com>
2024-08-02 13:41:03 +0200

    Made Local Species Input Windows draggable. Made close btn black across all browsers

--- src/InputWindow.jsx
+++ src/InputWindow.jsx
@@ -1,37 +1,41 @@
 import React, { useState } from 'react'
+import Draggable from "react-draggable";
 
 function InputWindow ( {handleCancel, objectType, speciesID, addNewObject} ) {
 
     const [inputFieldContent, setInputFieldContent] = useState('')
 
     return (
-        <div className="input-window">
+        <Draggable cancel='.input-window-content'>
+            <div className="input-window">
 
-            <div className='close-btn-container'>
-                <button className='close-btn' onClick={(event) => handleCancel(event, speciesID)}>✖</button>
-                <p className='window-header'>{objectType} Input</p>
+                <div className='close-btn-container'>
+                    <button className='close-btn' onClick={(event) => handleCancel(event, speciesID)}>✖</button>
+                    <p className='window-header'>{objectType} Input</p>
+                </div>
+
+                <div className='input-window-content'>
+                    <form
+                        className='input-window-form'
+                        onSubmit={ (event) => addNewObject(event, inputFieldContent, speciesID)}
+                    >
+                        <input
+                            className='input-field'
+                            type='text'
+                            required='required'
+                            pattern='^[^,]{1,45}$'
+                            title='No commas allowed. Max length 45 characters.'
+                            value={inputFieldContent}
+                            placeholder={`Add a new ${objectType}`}
+                            onChange={ (event) => setInputFieldContent(event.target.value) }
+                            autoFocus
+                        />
+                        <button className='input-window-submit-btn'>Submit</button>
+                    </form>
+                    <button className='input-window-cancel-btn' onClick={(event) => handleCancel(event, speciesID)}>Cancel</button>
+                </div>
             </div>
-
-            <form
-                className='input-window-form'
-                onSubmit={ (event) => addNewObject(event, inputFieldContent, speciesID)}
-            >
-                <input
-                    className='input-field'
-                    type='text'
-                    required='required'
-                    pattern='^[^,]{1,45}$'
-                    title='No commas allowed. Max length 45 characters.'
-                    value={inputFieldContent}
-                    placeholder={`Add a new ${objectType}`}
-                    onChange={ (event) => setInputFieldContent(event.target.value) }
-                    autoFocus
-                />
-                <button className='input-window-submit-btn'>Submit</button>
-            </form>
-
-            <button className='input-window-cancel-btn' onClick={(event) => handleCancel(event, speciesID)}>Cancel</button>
-        </div>
+        </Draggable>
     )
 
 }

64afff2bd6b9f7d1e144bdf163d7d250ebe6df82
AlonCohen96 <aloncohen36@gmail.com>
2024-08-11 16:39:48 +0200

    Cleaned up eventEmitter.js

--- src/eventEmitter.js
+++ src/eventEmitter.js
@@ -1,3 +1,9 @@
+/*
+This file sets up an emitter object.
+We use this in ScalableSpec.jsx to pass active label quickly from one track to another, this is important when dealing
+with thousands of labels simultaneously.
+ */
+
 import mitt from 'mitt';
 
 const emitter = mitt();

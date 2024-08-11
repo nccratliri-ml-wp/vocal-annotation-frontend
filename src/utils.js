f3564377cc13ab75d6255e4904a32d8595d70c63
AlonCohen96 <aloncohen36@gmail.com>
2024-08-11 17:25:44 +0200

    Cleaned up utils.js

--- src/utils.js
+++ src/utils.js
@@ -5,14 +5,4 @@ const excludeNonDigits = (event) => {
     }
 }
 
-const excludeSpecialCharacters = (event) => {
-    // Allow only letters, digits, hyphens, underscores, and dots
-    const allowedCharacters = /^[a-zA-Z0-9\-\_\.]$/
-
-    // Prevent the default behavior if the pressed key does not match the allowed characters
-    if (!allowedCharacters.test(event.key)) {
-        event.preventDefault()
-    }
-}
-
-export {excludeNonDigits, excludeSpecialCharacters}
+export {excludeNonDigits}
